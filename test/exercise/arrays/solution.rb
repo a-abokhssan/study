4088eafba7d0c4c44ff4463e842f7260bda249ae
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-09 17:42:16 +0300

    added replace function

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,8 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_num = array.reduce{|acc = 0, rec| rec > acc ? rec : acc}
+        array = array.map{|el| el > 0 ? el = max_num : el}
       end
 
       def search(_array, _query)
