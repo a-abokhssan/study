64c05235fb531cd7ecded74542b9690fe1e0ba81
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-11 15:10:55 +0300

    added search function

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -7,7 +7,18 @@ module Exercise
       end
 
       def search(_array, _query)
-        0
+        index = (_array.length - 1) / 2
+
+        return -1 if _array.length == 0 || (_array.length == 1 && _array[index] != _query)
+
+        if _query == _array[index]
+         index
+        elsif _query > _array[index]
+		      acc = search(_array[index + 1, _array.length], _query)
+		      acc == -1 ? -1 : index + 1 + acc
+        else
+          search(_array[0, index], _query)
+        end
       end
     end
   end
