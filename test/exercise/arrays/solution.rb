f48c4bf6240fe73dd64868a93beda47db0798d45
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-12 21:59:05 +0300

    fixed arguments name

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,18 +6,18 @@ module Exercise
         array = array.map{|el| el > 0 ? el = max_num : el}
       end
 
-      def search(_array, _query)
-        index = (_array.length - 1) / 2
+      def search(array, query)
+        index = (array.length - 1) / 2
 
-        return -1 if _array.length == 0 || (_array.length == 1 && _array[index] != _query)
+        return -1 if array.length == 0 || (array.length == 1 && array[index] != query)
 
-        if _query == _array[index]
-         index
-        elsif _query > _array[index]
-		      acc = search(_array[index + 1, _array.length], _query)
+        if query == array[index]
+          index
+        elsif query > array[index]
+		      acc = search(array[index + 1, array.length], query)
 		      acc == -1 ? -1 : index + 1 + acc
         else
-          search(_array[0, index], _query)
+          search(array[0, index], query)
         end
       end
     end
