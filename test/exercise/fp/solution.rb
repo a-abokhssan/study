b92c7349a1f51a47aad53e0983b1752bc02ba4bd
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-13 18:07:59 +0300

    added rating function

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,11 +1,13 @@
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(array)
 
+      def rating(array)
+        filtered_films = array.reject { |film| film['rating_kinopoisk'].to_f.nil? || film['rating_kinopoisk'].to_f.zero? }
+                              .reject { |film| film['country'] == nil || film['country'].split(',').length < 2 }
+        ratings = filtered_films.map { |film| film['rating_kinopoisk'].to_f}
+
+        ratings.reduce(:+) / ratings.length
       end
 
       def chars_count(films, threshold)
