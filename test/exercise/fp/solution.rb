72f680d47b308ef64664552e84c59f3f168db374
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-13 17:31:45 +0300

    added chars_count function

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,14 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.reject { |film| film["rating_kinopoisk"].to_f < threshold || film["rating_kinopoisk"].nil? }
+              .map { |film| film["name"] }
+              .reduce(0) { |result, name| result + name.count('и') }
       end
     end
   end
