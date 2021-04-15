73c85d8d55a6598a4b15719b51b5806f8817b3d2
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-15 17:47:50 +0300

    added my primitive function

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,20 +1,31 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      # Использовать стандартные функции массива для решения задач нельзя.
-      # Использовать свои написанные функции для реализации следующих - можно.
+      def my_each
+        index = 0
+        while index < length
+          yield self[index]
+          index += 1
+        end
+        self
+      end
 
-      # Написать свою функцию my_each
-      def my_each; end
+      def my_map
+        result = MyArray.new
+        my_each { |el| result << yield(el) }
+        result
+      end
 
-      # Написать свою функцию my_map
-      def my_map; end
+      def my_compact
+        result = MyArray.new
+        my_each { |el| result << el unless el.nil? }
+        result
+      end
 
-      # Написать свою функцию my_compact
-      def my_compact; end
-
-      # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        my_each { |el| acc = acc.nil? ? el : yield(acc, el) }
+        acc
+      end
     end
   end
 end
