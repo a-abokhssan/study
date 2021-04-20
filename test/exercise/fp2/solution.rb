ee7dc7facdd755f49f0de5705ee37c96901989b4
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-20 14:07:35 +0300

    fixed functions with recursion

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,30 +1,39 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      def my_each
-        index = 0
-        while index < length
-          yield self[index]
-          index += 1
+      def my_each(&blk)
+        first_elem = first
+        rest_elems = MyArray.new(self[1, length])
+        blk.call(first_elem)
+        if rest_elems.empty?
+          first_elem
+        else
+          rest_elems.my_each(&blk)
         end
         self
       end
 
-      def my_map
+      def my_map(&blk)
         result = MyArray.new
-        my_each { |el| result << yield(el) }
-        result
+        function = ->(acc, el) { acc << blk.call(el) }
+        MyArray.new(my_reduce(result, &function))
       end
 
       def my_compact
         result = MyArray.new
-        my_each { |el| result << el unless el.nil? }
-        result
+        function = ->(acc, el) { el.nil? ? acc : acc << el }
+        MyArray.new(my_reduce(result, &function))
       end
 
-      def my_reduce(acc = nil)
-        my_each { |el| acc = acc.nil? ? el : yield(acc, el) }
-        acc
+      def my_reduce(acc = nil, &blk)
+        first_elem = first
+        rest_elems = MyArray.new(self[1, length - 1])
+        if rest_elems.empty?
+          blk.call(acc, first_elem)
+        else
+          acc = acc.nil? ? first_elem : blk.call(acc, first_elem)
+          rest_elems.my_reduce(acc, &blk)
+        end
       end
     end
   end
